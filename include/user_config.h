7f0006fca305cb670f73a045b2a63cb134774f32
Alex Semenkov <asemenkov@onflydev.com>
2017-08-08 18:16:34 +0300

    Projects files added (to simplify import process)

--- include/user_config.h
+++ include/user_config.h
@@ -15,8 +15,8 @@
 #define WHIZZER_VERSION 	"0.3"
 
 #define ACTIVE_FLAG			1
-#define DEV_KEY				"7dad16ad7de7461cd02bc2e1211fd56e1a4173ec"
-#define TOKEN				"4537e2fec21cf9a1b6e1589cf6ccbb79a58e375e"
+#define DEV_KEY				"40-chars Device Key from IoT cloud (master)"
+#define TOKEN				"40-chars Device Key from IoT cloud (owner)"
 
 #define ESP_DOMAIN      	"iot.espressif.cn"
 #define BEACON_TIME     	50000
